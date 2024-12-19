86e0c97a3c5ab4a8a83d8dfd1eec8d2ef6e8f7a8
banhsz <kozikbro@gmail.com>
2024-12-19 21:25:50 +0100

    initial

---
+++ js/site.js
@@ -0,0 +1,7 @@
+$(function(){
+    // Hide navbar when menu item is clicked
+    var navMain = $(".navbar-collapse");
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+});
